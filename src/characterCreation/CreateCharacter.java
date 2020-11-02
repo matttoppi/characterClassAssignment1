350f23c4313e706c188dc86733bc4d25c8dcde7a
Matt Toppi <mdtoppi7442@eagle.fgcu.edu>
2020-11-01 20:46:26 -0500

    copied professors code from video added methods for: taking damage getting healed exiting the game changing levels

---
+++ src/characterCreation/CreateCharacter.java
@@ -0,0 +1,10 @@
+package characterCreation;
+
+public class CreateCharacter {
+
+    public static void main(String[] args) {
+     Character player1 = new Character("Link", "dwarf", "barbarian");
+     System.out.println(player1.toString());
+    }
+
+}
